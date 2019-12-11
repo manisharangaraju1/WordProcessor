3358a786aa04ce2adf355f306982bfc01ce5c084
MRANGARAJU <manisha.rangaraju@gmail.com>
2019-12-11 13:05:55 -0800

    Compare in tests

--- src/WordProcessor.java
+++ src/WordProcessor.java
@@ -1,6 +1,4 @@
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class WordProcessor {
 
@@ -16,35 +14,5 @@ public class WordProcessor {
             Font textFont = runArray.getFont(textIndex);
             unicode.setFont(textFont);
         }
-    }
-
-    public static void main(String[] args) {
-        double memory = new SizeofUtil(){
-            @Override
-            protected int create() {
-                FontFactory fontFactory = FontFactory.getFontFactoryInstance();
-                WordProcessor wordProcessor = new WordProcessor(CharacterFactory.getTextCharacterFactoryInstance());
-                RunArray runArray = new RunArray();
-                Font fontOne = fontFactory.getFont("TIMES NEW ROMAN", Font.ITALIC, 10);
-                runArray.addRun(0, 14, fontOne);
-                wordProcessor.parseText("ABC ABCDABCABC", runArray);
-                return 1;
-            }
-        }.averageBytes();
-        System.out.println("With flyweight : " + memory);
-
-        double memoryWithoutFlyweight = new SizeofUtil(){
-            @Override
-            protected int create() {
-                List<NonFlyweightUnicode> characterArray = new ArrayList<>();
-                String text = "ABC ABCDABCABC";
-                for (char c : text.toCharArray()) {
-                    Font fontOne = FontFactory.getFontFactoryInstance().getFont("TIMES NEW ROMAN", Font.ITALIC, 10);
-                    characterArray.add(new NonFlyweightUnicode(c, fontOne));
-                }
-                return 1;
-            }
-        }.averageBytes();
-        System.out.println("memoryWithoutFlyweight : " + memoryWithoutFlyweight);
     }
 }
--- src/WordProcessorTest.java
+++ src/WordProcessorTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
 
@@ -14,15 +16,27 @@ class WordProcessorTest {
                 WordProcessor wordProcessor = new WordProcessor(CharacterFactory.getTextCharacterFactoryInstance());
                 RunArray runArray = new RunArray();
                 Font fontOne = FontFactory.getFontFactoryInstance().getFont("TIMES NEW ROMAN", Font.ITALIC, 10);
-                Font fontTwo = FontFactory.getFontFactoryInstance().getFont("ARIAL", Font.BOLD, 12);
                 runArray.addRun(0, 3, fontOne);
-                runArray.addRun(4, 5, fontTwo);
                 runArray.appendRun(11, fontOne);
                 wordProcessor.parseText("ABC ABCDABCABC", runArray);
                 return 1;
             }
         }.averageBytes();
-        assertTrue(memory == 1668.0);
+
+        double memoryWithoutFlyweight = new SizeofUtil(){
+            @Override
+            protected int create() {
+                List<NonFlyweightUnicode> characterArray = new ArrayList<>();
+                String text = "ABC ABCDABCABC";
+                for (char c : text.toCharArray()) {
+                    Font fontOne = FontFactory.getFontFactoryInstance().getFont("TIMES NEW ROMAN", Font.ITALIC, 10);
+                    characterArray.add(new NonFlyweightUnicode(c, fontOne));
+                }
+                return 1;
+            }
+        }.averageBytes();
+
+        assertTrue(memoryWithoutFlyweight < memory);
     }
 
 
